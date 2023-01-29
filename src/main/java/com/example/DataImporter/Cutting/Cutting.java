4c5d597a53d2e9d047943343a314cb509a19e2d5
Mateusz Konopka <mkonopkadev@gmail.com>
2023-01-29 19:11:17 +0100

    initial & entity added

---
+++ src/main/java/com/example/DataImporter/Cutting/Cutting.java
@@ -0,0 +1,48 @@
+package com.example.DataImporter.Cutting;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cutting_details")
+public class Cutting {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "int_ext")
+    private String intExt;
+    @Column(name = "sq_ang")
+    private String sqAng;
+    @Column(name = "ref")
+    private String ref;
+    @Column(name = "material")
+    private String material;
+    @Column(name = "grade")
+    private String grade;
+    @Column(name = "width")
+    private int width;
+    @Column(name = "height")
+    private int height;
+    @Column(name = "length")
+    private int length;
+    @Column(name = "left_cut")
+    private float leftCut;
+    @Column(name = "right_cut")
+    private float rightCut;
+    @Column(name = "qty")
+    private int qty;
+    @Column(name = "set_ref")
+    private String setRef;
+
+}
---
+++ src/test/java/com/example/DataImporter/DataImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataImporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
